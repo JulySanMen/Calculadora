169ca1421eeab5aebf0748c29636c2bd94bbfe91
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2024-10-18 03:38:29 +0000

    resta

--- calcu.js
+++ calcu.js
@@ -5,3 +5,9 @@ function suma(a, b) {
 
 let resultado = suma(5, 3); // Definir resultado
 console.log(resultado); // Mostrar el resultado
+
+function resta(a, b) {
+    return a - b;
+}
+let resultadores = resta(9, 3);
+console.log(resultadores);
