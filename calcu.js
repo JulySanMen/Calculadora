933b2464e8199043a86ec7f52bf2647461fd0a57
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2024-10-20 22:51:16 +0000

    multiplica

--- calcu.js
+++ calcu.js
@@ -11,3 +11,10 @@ function resta(a, b) {
 }
 let resultadores = resta(9, 3);
 console.log(resultadores);
+
+function multi(a,b){
+    return a*b;
+}
+let resultadomulti =multi(5,5);
+console.log(resultadomulti);
+
