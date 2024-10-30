085d3d03cdcfc38c28277b92c3274894015dede0
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2024-10-30 04:51:56 +0000

    numeroMaximo

---
+++ numMax.js
@@ -0,0 +1,16 @@
+//Encontrar el numro mayor de una lista de numeros en un array
+//defenir nuestro arreglo
+
+let numeros = [2, 3, 6, 1, 8, 4];
+
+//variable para nuetsro bjetivo, iniciar en 0
+
+let numMax = 0;
+let tamanoarray = numeros.length;
+
+for (let i = 0; i < tamanoarray; i++) {
+  if (numMax < numeros[i]) {
+    numMax = numeros[i];
+  }
+}
+console.log(numMax);
