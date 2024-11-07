792bddf52d7e515b8e6b0c041789bc73f2382ee8
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2024-11-07 04:42:11 +0000

    climaphp

--- clima.php
+++ clima.php
@@ -5,3 +5,5 @@ $ubication = array ("Guajira"=> "Norte", "Leticia"=>"Sur", "Santander"=>"Este",
 "Antioquia"=>"Oeste");
 $tourism = array("Santa Marta"=>"Mar", "Villavicencio"=>"Llano", "Rioacha"=>"Desierto", 
 "Quindio"=>"Valle" );
+
+$search = readline("Seleccione la opción de su interes:   1.Clima 2.Ubicación 3.Turismo  ");
