4e4e277a15eefb1a3600f96f270354bea62043e6
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2024-11-07 04:40:16 +0000

    buscadorphp

---
+++ clima.php
@@ -0,0 +1,7 @@
+// pediremos recomendaciones de lugares de Colombia de acuerdo con su clima, ubicación o atractivo turístico
+
+$weather = array("Bogota" => "Frio", "Monteria" => "Calido", "Medellin" => "Templado" );
+$ubication = array ("Guajira"=> "Norte", "Leticia"=>"Sur", "Santander"=>"Este", 
+"Antioquia"=>"Oeste");
+$tourism = array("Santa Marta"=>"Mar", "Villavicencio"=>"Llano", "Rioacha"=>"Desierto", 
+"Quindio"=>"Valle" );
