6ffe4b312157bd3982cf5ddffc277f785fa57635
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2025-01-29 22:46:05 -0500

    Update cortaString.py

--- cortaString.py
+++ cortaString.py
@@ -1,8 +1,8 @@
 #Extraer un substring 
 
 oracion = "se acercan tiempos obscuros"
-puntoinicio = 10
-puntofinal = 17
+puntoinicio = 12
+puntofinal = 27
 
 palabraCorte = oracion[puntoinicio:puntofinal]
 print(palabraCorte)
